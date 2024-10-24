63a421d5d06df9663e4e4a45b0f6307090e5aa86
haruka23012004 <eangelicapalomino@gmail.com>
2024-10-24 11:32:28 -0500

    insertar

--- insertar.php
+++ insertar.php
@@ -2,15 +2,13 @@
 
 include 'conexion.php';
 
-// Inicializar 
-
+// Inicializar
 $mensaje = '';
 $error = '';
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-
+    
     $nombre_completo = $_POST['nombre_completo'];
     $direccion = $_POST['direccion'];
     $observacion = $_POST['observacion'];
@@ -18,27 +16,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     if (!empty($nombre_completo) && !empty($direccion) && !empty($observacion)) {
 
-        // verificar si el registro ya existe 
-
+        // Verificar si el registro ya existe
         $sql_check = "SELECT * FROM tabla WHERE nombre_completo = ? AND direccion = ? AND observacion = ?";
         if ($stmt_check = mysqli_prepare($conn, $sql_check)) {
             mysqli_stmt_bind_param($stmt_check, "sss", $nombre_completo, $direccion, $observacion);
             mysqli_stmt_execute($stmt_check);
             $result_check = mysqli_stmt_get_result($stmt_check);
 
-            // Si se encuentra da un mensaje de error
+            // mensaje de error
             if (mysqli_num_rows($result_check) > 0) {
                 $error = "El registro ya existe";
-            } 
-
-            else {
-                // Si no existe, insertar los datos
+            } else {
+                // Si no existe insertar los datos
                 $sql = "INSERT INTO tabla (nombre_completo, direccion, observacion) VALUES (?, ?, ?)";
                 if ($stmt = mysqli_prepare($conn, $sql)) {
                     mysqli_stmt_bind_param($stmt, "sss", $nombre_completo, $direccion, $observacion);
                     if (mysqli_stmt_execute($stmt)) {
-                        // Mostrar mensaje de éxito
-                        $mensaje = "Registro insertado correctamente.";
+
+                        // Redirigir al index.php 
+                        header("Location: index.php");
+                        exit(); // Detener la ejecución del script 
                     } else {
                         $error = "Error al insertar los datos: " . mysqli_error($conn);
                     }
@@ -54,8 +51,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-
 mysqli_close($conn);
-
-
-    
