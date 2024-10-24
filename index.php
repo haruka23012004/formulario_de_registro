844c6582120fc61f9b47ad40d69e6453b65b74a9
haruka23012004 <eangelicapalomino@gmail.com>
2024-10-24 17:17:30 -0500

    cambio final

--- index.php
+++ index.php
@@ -87,7 +87,8 @@
     </div>
 
     <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- lo necesito para poder usar datatables -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>  
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
     <script>
